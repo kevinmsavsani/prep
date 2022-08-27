ee2516394aee42531e93d19f934741d8e0b0b080
Kevin Savsani <32890046+kevinmsavsani@users.noreply.github.com>
2022-08-27 12:29:33 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var users = {
+    "user1" : {
+       "name" : "mahesh",
+       "password" : "password1",
+       "profession" : "teacher",
+       "id": 1
+    },
+ }
+
+app.get('/', function (req, res) {
+      res.send( users );
+})
+
+var server = app.listen(8081, function () {
+   console.log("Example app listening at http://localhost:8081")
+})
+
+//command to start - node start
